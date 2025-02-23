359cfb35ad04b1c03fb70dae8854b57ccaa50661
Unity763 <unitylk51@gmail.com>
2025-02-23 12:40:16 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*LORA [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
-            const dt = await RobinPairWeb.sendMessage(user_jid, {
+            const dt = await LoraPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22AE333B47-DFC4-4FD9-9421-FB3E4D0F5466.jpeg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-02-23T07%3A03%3A59.500Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F8c07e25dd4144746%2FAE333B47-DFC4-4FD9-9421-FB3E4D0F5466.jpeg%3FExpires%3D1834902240%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DTLc7BKbk8W~ji6JVjEjFrJiTBVN2CqHQe6xFHNUoCSwYst6DctKA9LMW1UyV0hKJFpxbFZffDGr7BIT~whaqT4RWbK67q94Lp7p4~FNjFCYwsa277toPfK~~3EcnahSbHSQ8gvT2qloZk7pxirLutnVUKqEbU6XjphMxX4meZ43Qgnb0Hbc038pDlhu0dKiW1DmIgGRuqgHiADZP-npJ2DFh09McaRafEGvvL~~iUqRz5683pyU3FhPm1kANym94CJen7qTK7yUY~x4RTy2fKNxN7G6sD9PP5VXTogMc06u459769XLWDfx-6Wsk-tzK31PhTIddTdTGXK-6GXYEBg__%22%7D",
               },
               caption: sid,
             });
